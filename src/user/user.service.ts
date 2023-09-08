39bcc4d4fb691028bcd3ef40f439f40a303daed3
yisuho <yeek0620@gmail.com>
2023-09-08 19:15:43 +0900

    commit

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -23,7 +23,7 @@ export class UserService {
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} user`;
+    return `This action returns a #${id} user .`;
   }
 
   update(id: number, updateUserDto: UpdateUserDto) {
